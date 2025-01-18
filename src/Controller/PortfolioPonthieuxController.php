dcca9d4f2f89d394eab515222423a0ebbf665994
ShyeTF <leoponthieux74@gmail.com>
2025-01-19 00:43:11 +0100

    patch

--- src/Controller/PortfolioPonthieuxController.php
+++ src/Controller/PortfolioPonthieuxController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request; // Ajout ou vérification
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class PortfolioPonthieuxController extends AbstractController{
+final class PortfolioPonthieuxController extends AbstractController
+{
     #[Route('/home', name: 'home')]
     public function home(): Response
     {
@@ -18,7 +20,7 @@ final class PortfolioPonthieuxController extends AbstractController{
     #[Route('/cv', name: 'cv')]
     public function cv(): Response
     {
-    return $this->render('portfolio_ponthieux/cv.html.twig');
+        return $this->render('portfolio_ponthieux/cv.html.twig');
     }
 
     #[Route('/portfolio', name: 'portfolio')]
@@ -32,8 +34,31 @@ final class PortfolioPonthieuxController extends AbstractController{
     #[Route('/passions', name: 'passions')]
     public function passions(): Response
     {
-    return $this->render('portfolio_ponthieux/passions.html.twig');
-    }  
+        return $this->render('portfolio_ponthieux/passions.html.twig');
+    }
+
+    #[Route('/projets', name: 'projets')]
+    public function projets(): Response
+    {
+        return $this->render('portfolio_ponthieux/projets.html.twig', [
+            'controller_name' => 'PortfolioPonthieuxController',
+        ]);
+    }
+
+    #[Route('/loisirs', name: 'loisirs')]
+    public function loisirs(): Response
+    {
+        return $this->render('portfolio_ponthieux/loisirs.html.twig', [
+            'controller_name' => 'PortfolioPonthieuxController',
+        ]);
+    }
+
+    #[Route('/cv_form', name: 'cv_form')]
+    public function cv_form(): Response
+    {
+        return $this->render('portfolio_ponthieux/cv_form.html.twig', [
+            'controller_name' => 'PortfolioPonthieuxController',
+        ]);
+    }
 
 }
-
