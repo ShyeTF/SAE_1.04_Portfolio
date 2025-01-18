7ea4eb4481b36f5b96a807f9ca28bc0ba335b28a
ShyeTF <leoponthieux74@gmail.com>
2025-01-18 14:51:37 +0100

    Initial commit

---
+++ src/Controller/PortfolioPonthieuxController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PortfolioPonthieuxController extends AbstractController{
+    #[Route('/home', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('portfolio_ponthieux/home.html.twig', [
+            'controller_name' => 'PortfolioPonthieuxController',
+        ]);
+    }
+
+    #[Route('/portfolio', name: 'portfolio')]
+    public function monportfolio(): Response
+    {
+        return $this->render('portfolio_ponthieux/portfolio.html.twig', [
+            'controller_name' => 'PortfolioPonthieuxController',
+        ]);
+    }
+
+}
+
